36849dbdf0d2bd3994e4612a2909e5d34d73d92e
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-08-29 20:32:52 +0300

    233. Displaying a Map Using Leaflet Library

--- script.js
+++ script.js
@@ -12,18 +12,34 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
+  navigator.geolocation.getCurrentPosition(
 
     (position) => {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
     //   console.log(latitude, longitude);
     console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, latitude]
+
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    // L.marker([51.5, -0.09])
+    L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
     }, () => {
     
     }
-    );
+  );
 }
 
 
 //  Using the Geolocation API
+// 233. Displaying a Map Using Leaflet Library
